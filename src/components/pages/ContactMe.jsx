cb669e6707bfb0d28ed156aeb9da9f73affd70be
nvanschaack <nellvanschaack@gmail.com>
2024-06-10 14:47:57 -0500

    deleted extra code in contactme.jsx

--- src/components/pages/ContactMe.jsx
+++ src/components/pages/ContactMe.jsx
@@ -1,71 +1,34 @@
 import React, { useState } from 'react'
-import {Form, Col, Row, Button} from 'react-bootstrap'
+import { Form, Col, Row, Button } from 'react-bootstrap'
 
 export default function ContactMe() {
-  
-    //contactFormData is the current value and setContactFormData is the updated value. contactFormData refers to the initial value of name, email, and message. setContactFormData is what is input by the user in the form's input fields
-    const [contactFormData, setContactFormData] = useState({ name: '', email: '', message: '' });
-    // const [formErrors, setFormErrors] = useState({});
 
-    const handleChanges = (e) => {
-      //this is spreading contactFormData (an object) so that the target.name (stands for either the name, email, or message) is set to the value that the user types in the input fields. Then this function will give new values to the setContactFormData
-      setContactFormData({ ...contactFormData, [e.target.name]: e.target.value })
-    };
+  //contactFormData is the current value and setContactFormData is the updated value. contactFormData refers to the initial value of name, email, and message. setContactFormData is what is input by the user in the form's input fields
+  const [contactFormData, setContactFormData] = useState({ name: '', email: '', message: '' });
+  // const [formErrors, setFormErrors] = useState({});
 
-    // const handleBlurForm = (e) => {
-    //   if (!e.target.value) {
-    //     setFormErrors({ ...formErrors, [e.target.name]: 'This needs to be filled out' })
-    //   } else {
-    //     setFormErrors({ ...formErrors, [e.target.name]: '' })
-    //   }
-    // };
+  const handleChanges = (e) => {
+    //this is spreading contactFormData (an object) so that the target.name (stands for either the name, email, or message) is set to the value that the user types in the input fields. Then this function will give new values to the setContactFormData
+    setContactFormData({ ...contactFormData, [e.target.name]: e.target.value })
+  };
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      console.log(contactFormData);
+  // const handleBlurForm = (e) => {
+  //   if (!e.target.value) {
+  //     setFormErrors({ ...formErrors, [e.target.name]: 'This needs to be filled out' })
+  //   } else {
+  //     setFormErrors({ ...formErrors, [e.target.name]: '' })
+  //   }
+  // };
 
-      setContactFormData({name: '', email: '', message: ''})
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(contactFormData);
 
-    return (
-      // <section>
-      //   <h2>Contact Me</h2>
-      //   <form onSubmit={handleSubmit}>
-      //     <div>
-      //       <label>Full Name</label>
-      //       <input
-      //         type="text"
-      //         name="name"
-      //         value={contactFormData.name}
-      //         onChange={handleChanges}
-      //         // onBlur={handleBlurForm}
-      //       />
-      //     </div>
-      //     <div>
-      //     <label>Email:</label>
-      //       <input
-      //         type="email"
-      //         name="email"
-      //         value={contactFormData.email}
-      //         onChange={handleChanges}
-      //         // onBlur={handleBlurForm}
-      //       />
-      //     </div>
-      //     <div>
-      //     <label>Email Message:</label>
-      //       <input
-      //         type="text"
-      //         name="message"
-      //         value={contactFormData.message}
-      //         onChange={handleChanges}
-      //         // onBlur={handleBlurForm}
-      //       />
-      //     </div>
-      //     <Button variant="secondary" type='submit'>Submit</Button>
-      //   </form>
-      // </section>
+    setContactFormData({ name: '', email: '', message: '' })
+  };
 
-<section style={{ border: '1px solid blue', padding: '20px', margin: '40px' }}>
+  return (
+    <section style={{ border: '1px solid blue', padding: '20px', margin: '40px' }}>
       <h2>Contact Me</h2>
       <Row className="justify-content-md-center">
         <Col xs={12} md={8}>
@@ -108,7 +71,7 @@ export default function ContactMe() {
         </Col>
       </Row>
     </section>
-    )
-  
+  )
+
 }
 
