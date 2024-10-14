c311b6433550abb409e6f7bdcd535ebdc43d5415
nvanschaack <nellvanschaack@gmail.com>
2024-10-14 18:36:04 -0500

    edited p tag in aboutme.jsx

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -14,7 +14,7 @@ export default function AboutMe() {
                     />
                 </Col>
                 <Col className="text-center d-flex align-items-center" >
-                    <p>After graduating from Georgetown University in 2021 with a Bachelor of Science in Nursing, I honed my problem-solving and critical thinking skills through 2.5 years as a registered nurse. Now, I'm passionate about applying those skills to the world of software development. Having completed a full-stack web development bootcamp at The University of Texas, I'm eager to learn and grow as a software engineer, utilizing my existing skillset and embracing the challenges of my next role.</p>
+                    <p>After graduating from Georgetown University in 2021 with a Bachelor of Science in Nursing, I honed my problem-solving and critical thinking skills through 3 years as a registered nurse. Now, I'm passionate about applying those skills to the world of software development. Having completed a full-stack web development bootcamp at The University of Texas, I'm eager to learn and grow as a software engineer, utilizing my existing skillset and embracing the challenges of my next role.</p>
                 </Col>
             </Row>
         </Container>
