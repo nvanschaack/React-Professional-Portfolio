f29e314d17eb16f19cb26764f433dd7c5f5ebc4f
nvanschaack <nellvanschaack@gmail.com>
2024-11-07 15:31:35 -0600

    updated resume, about me section, and about me picture

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -1,22 +1,47 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap';
-import profile from '../../assets/images/nell.jpeg'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import profile from "../../assets/images/nell.jpeg";
 
 export default function AboutMe() {
-    return (
-        <Container className="about-me-container"> {/* Add a class for potential styling */}
-            <Row>
-                <Col className='text-center'>
-                    <img
-                        src={profile}
-                        alt="Picture of me"
-                        style={{ height: '500px', borderRadius: '10px' }}
-                    />
-                </Col>
-                <Col className="text-center d-flex align-items-center" >
-                    <p>After graduating from Georgetown University in 2021 with a Bachelor of Science in Nursing, I honed my problem-solving and critical thinking skills through 3 years as a registered nurse. Now, I'm passionate about applying those skills to the world of software development. Having completed a full-stack web development bootcamp at The University of Texas, I'm eager to learn and grow as a software engineer, utilizing my existing skillset and embracing the challenges of my next role.</p>
-                </Col>
-            </Row>
-        </Container>
-    )
+  return (
+    <Container className="about-me-container">
+      {" "}
+      {/* Add a class for potential styling */}
+      <Row>
+        <Col className="text-center">
+          <img
+            src={profile}
+            alt="Picture of me"
+            style={{ height: "500px", borderRadius: "10px" }}
+          />
+        </Col>
+        <Col className="text-center d-flex align-items-center">
+          <p>
+            As a retired Division I athlete from Georgetown University, I hold a
+            Bachelor of Science in Nursing and have spent my early career as a
+            Registered Nurse, where I developed a strong passion for
+            helping others.
+          </p>
+
+          <p>
+            After three fulfilling years in nursing, I sought a new challenge
+            that would allow me to leverage my analytical skills and creativity.
+            Encouraged by discussions with professionals who successfully
+            transitioned to new careers, I discovered the field of web
+            development. My early exposure to programming in a statistics class
+            sparked an enthusiastic interest that led me to enroll in a web
+            development bootcamp at the University of Texas.
+          </p>
+
+          <p>
+            This decision marked a pivotal shift in my career journey, and I
+            have embraced the opportunity to create impactful solutions in a
+            dynamic industry. Now, I am eager to apply my diverse experiences
+            and skills to meaningful projects that continue to support and
+            uplift individuals in innovative ways.
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import pdf from '../../assets/resume.pdf'
+import React from "react";
+import pdf from "../../assets/resume.pdf";
 
 export default function Resume() {
   return (
-    <div className='m-5'>
-      {/* create a list group with react bootstrap for skills*/}
+    <div className="m-5">
       <p>
-        If you'd like to check out my resume, click the button below.
+        <strong>Technical Skills:</strong> JavaScript | React | HTML | CSS |
+        MySQL | Git | Node | Express | Bootstrap | MongoDB | GraphQL
       </p>
+      <p>If you'd like to check out my resume, click the button below.</p>
       <a href={pdf} download>
         <button>download resume</button>
       </a>
     </div>
-
-  )
+  );
 }
